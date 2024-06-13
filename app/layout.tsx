04498c769900c3de987c82b0815933d567b7dd1d
Matthieu-Vachet <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-06-13 17:14:04 +0200

    - Modification du globe pour de meilleur performance - Suppression de class css non utiliser dans global css - Passage des import section dans layout en Lazy et suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="fr">
+        <html lang="fr" suppressHydrationWarning>
             <head>
                 <meta
                     name="google-site-verification"
