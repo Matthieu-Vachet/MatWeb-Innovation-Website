8980a4208134fb887b6f1ddb0aa4075f53d75a18
Matthieu-Vachet <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-08-01 18:42:47 +0200

    Modification divers

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
     return (
         <section
             id="not-found"
-            className=" w-screen h-screen flex bg-gradient-to-b from-purple-950/15 to-black-100"
+            className="w-screen h-screen flex bg-gradient-to-b from-purple-950/15 to-black-100"
         >
             <div className="w-screen h-full flex flex-col justify-center items-center text-center">
                 <Image
@@ -22,25 +22,25 @@ export default function NotFound() {
                     layout="responsive"
                 />
                 {/* Background Grid */}
-                <div className="h-[80vh]  w-full bg-grid-white/[0.04] flex items-center justify-center absolute top-0 left-0 mt-0 mb-0 pt-0 pb-0 z-0">
+                <div className="h-[80vh] w-full bg-grid-white/[0.04] flex items-center justify-center absolute top-0 left-0 mt-0 mb-0 pt-0 pb-0 z-0">
                     <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-gradient-to-b from-purple-950/10 to-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,white)]" />
                 </div>
 
-                <div className="flex justify-center relative z-10">
+                <div className="flex justify-center items-center relative z-10 -mt-28 lg:-mt-0 ">
                     <div className="max-w-[90vw] md:max-w-[80vw] lg:max-w-[80vw] flex flex-col items-center justify-center gap-10">
-                        <h1 className="uppercase tracking-widest text-3xl md:text-5xl lg:text-6xl imac:text-7xl text-center text-white-200 translate-y-[-1rem] animate-fade-in opacity-0">
-                            Page not found
+                        <h1 className="uppercase tracking-widest text-xl md:text-4xl lg:text-5xl imac:text-6xl text-center text-white-200 translate-y-[-1rem] animate-fade-in opacity-0">
+                            La page demandée n&apos;est pas disponible
                         </h1>
-                        <div className="text-center uppercase translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:100ms] lg:max-w-[75vw] border-t-4 border-b-4 border-white-100 p-5">
+                        <div className="text-center uppercase translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:100ms] lg:max-w-[75vw] border-t-4 border-b-4 border-white-100 pt-5">
                             <h2 className="text-3xl md:text-5xl lg:text-6xl imac:text-7xl">
                                 Erreur
                             </h2>
-                            <h3 className="text-3xl md:text-5xl lg:text-6xl imac:text-[15rem] font-semibold bg-gradient-to-r from-purple to-white-200 bg-clip-text text-transparent">
+                            <h3 className="text-[5rem] md:text-[9rem] lg:text-[9rem] imac:text-[15rem] font-semibold bg-gradient-to-r from-purple to-white-200 bg-clip-text text-transparent">
                                 404
                             </h3>
                         </div>
 
-                        <p className="text-3xl md:text-4xl lg:text-4xl font-normal text-white-200 text-center translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+                        <p className="text-xl md:text-3xl lg:text-3xl font-normal text-white-200 text-center translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
                             Si vous avez saisi l&apos;URL directement, veuillez vérifier
                             l&apos;orthographe.
                         </p>
